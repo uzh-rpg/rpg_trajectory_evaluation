16b6bdf7b9c17e8f0b304c5e4955e2004547599f
Rik Bähnemann <brik@ethz.ch>
2021-01-06 10:33:05 +0100

    Support Odometry and TransformStamped types.

--- scripts/dataset_tools/bag_to_pose.py
+++ scripts/dataset_tools/bag_to_pose.py
@@ -12,7 +12,8 @@ def extract(bagfile, pose_topic, msg_type, out_filename):
     f.write('# timestamp tx ty tz qx qy qz qw\n')
     with rosbag.Bag(bagfile, 'r') as bag:
         for (topic, msg, ts) in bag.read_messages(topics=str(pose_topic)):
-            if msg_type == "PoseWithCovarianceStamped":
+            if msg_type == "PoseWithCovarianceStamped" or \
+               msg_type == "Odometry":
                 f.write('%.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f\n' %
                         (msg.header.stamp.to_sec(),
                          msg.pose.pose.position.x, msg.pose.pose.position.y,
@@ -28,6 +29,14 @@ def extract(bagfile, pose_topic, msg_type, out_filename):
                          msg.pose.position.z,
                          msg.pose.orientation.x, msg.pose.orientation.y,
                          msg.pose.orientation.z, msg.pose.orientation.w))
+            elif msg_type == "TransformStamped":
+                f.write('%.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f\n' %
+                        (msg.header.stamp.to_sec(),
+                         msg.transform.translation.x,
+                         msg.transform.translation.y,
+                         msg.transform.translation.z,
+                         msg.transform.rotation.x, msg.transform.rotation.y,
+                         msg.transform.rotation.z, msg.transform.rotation.w))
             else:
                 assert False, "Unknown message type"
             n += 1
